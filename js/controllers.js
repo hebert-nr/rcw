983db3a8b396053b671db0392190e8ae232e17f1
hebert-nr <nheb461@outlook.com>
2016-08-12 10:32:36 -0700

    Initial creation of rcw git

---
+++ js/controllers.js
@@ -0,0 +1,52 @@
+
+/*
+var myApp will contain all our code for the application. aka NameSpacing. and this will need to be pointed to from our HTML file, in the HTML tag.
+ Inside the [] are directives.
+ $scope is like a super-variable we can pass things from our JS to our app and template, and vice-versa
+*/
+var myApp = angular.module('myApp',['ui.bootstrap']);
+
+/* We pass the scope and http in an array to keep from getting messed up by minification of css/js*/
+myApp.controller('MyController', ['$scope', '$http', function ($scope, $http) {
+	
+	
+/*We can also pass in external files rather than hard coding using the HTTP service. It pulls the data.js file containing all our JSON rather than having these 
+hundreds of lines being hardcoded here in our controller.*/	
+$http.get('js/data.json').success(function(data){
+$scope.rcw = data;	
+})
+
+	
+myApp.controller('NavController', function(){
+		this.tab = 1;
+		
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		};
+		
+		this.isSelected= function(checkTab){
+			return this.tab === checkTab;
+		};
+	});
+
+	
+}]);
+
+
+/*Helps filter to a 1 unique item we're searching for, sort of like DISTINCT in SQL*/
+myApp.filter('unique', function() {
+   return function(collection, keyname) {
+      var output = [], 
+          keys = [];
+
+      angular.forEach(collection, function(item) {
+          var key = item[keyname];
+          if(keys.indexOf(key) === -1) {
+              keys.push(key);
+              output.push(item);
+          }
+      });
+
+      return output;
+   };
+});
